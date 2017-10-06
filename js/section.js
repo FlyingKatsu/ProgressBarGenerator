385ee6a79bbcdf798883ab4ac3ef5e9c8a468234
Flying Katsu <Warped2713@users.noreply.github.com>
2017-10-06 17:58:50 -0400

    initial data models for section.js

--- js/section.js
+++ js/section.js
@@ -44,7 +44,58 @@ var togglePrev = function(evt, n, label) {
 //   DATA MODEL
 // =============================================================
 
+const StrokeObj = function(_input) {
+  let input = _input || {};
+  this.width = _input.width || 1;
+  this.color = _input.color || '#000000';
+  this.alpha = _input.alpha || 1;
+};
 
+const FillObj = function(_input) {
+  let input = _input || {};
+  this.color = _input.color || '#ffffff';
+  this.alpha = _input.alpha || 0.5;
+  this.direction = _input.direction || 'vertical';
+  this.gradient = [
+    { color: '#000000', alpha: 1, loc: 0.5 },
+    { color: '#ffffff', alpha: 1, loc: 0.75 }
+  ];
+};
+
+const FontObj = function(_input) {
+  let input = _input || {};
+  this.size = _input.size || 64;
+  this.family = _input.family || 'sans-serif';
+  this.alignment = _input.alignment || 'center';
+  this.baseline = _input.baseline || 'alphabetic';
+  this.stroke = _input.stroke || new StrokeObj();
+  this.fill = _input.fill || new FillObj();
+};
+
+const PolyObj = function(_input) {
+  let input = _input || {};
+  this.width = _input.width || 128;
+  this.height = _input.height || 32;
+  this.shape = _input.shape || 'rectangular';
+  this.stroke = _input.stroke || new StrokeObj();
+  this.fill = _input.fill || new FillObj();
+};
+
+const ImgObj = function(_input) {
+  let input = _input || {};
+  this.data = _input.data || null;
+  this.width = _input.width || 128;
+  this.height = _input.height || 128;
+  this.crop = _input.crop || { x: 0, y: 0, w: 128, h: 128 };
+};
+
+const MODEL = {
+  Font: FontObj,
+  Img: ImgObj,
+  Poly: PolyObj,
+  Stroke: StrokeObj,
+  Fill: FillObj
+};
 
 // =============================================================
 //   DRAW METHODS
