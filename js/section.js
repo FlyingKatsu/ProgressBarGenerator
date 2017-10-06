142ae578af6793393e9de4bc1bab7dd1bbd6b761
Flying Katsu <Warped2713@users.noreply.github.com>
2017-10-06 19:16:54 -0400

    creategradient and hex2rgba helpers

--- js/section.js
+++ js/section.js
@@ -44,22 +44,60 @@ var togglePrev = function(evt, n, label) {
 //   DATA MODEL
 // =============================================================
 
+/**
+ * The Gradient Factory (use in draw methods when gradient data exists).
+ * Returns a Context2D Gradient object with _input data.
+ * Defaults to evenly-spaced vertical clear-to-black 100px gradient.
+ * @param {*} _input The data specifying a gradient
+ * @param {*} ctx A Canvas Context2D
+ */
+const CreateGradient = function(_input, ctx) {
+  // Set up defaults
+  let input = _input || {};
+  data.shape = _input.shape || 'linear'; // radial or linear
+  data.colors = _input.colors || [];
+  data.start = _input.start || { x: 0, y: 0, r: 0 };
+  data.end = _input.end || { x: 100, y: 0, r: 0 };
+  // Create the gradient shape as radial or linear
+  let gradient = (data.shape == 'radial') ? ctx.createRadialGradient() : ctx.createLinearGradient();
+  // Add the color stops
+  this.colors.map(function(color) {
+    gradient.addColorStop(color.loc, color.value);
+  });
+  return gradient;
+}
+
+/**
+ * Convert a hexidecimal color string into rgba string
+ * @param {string} hex A string of six characters 0-9a-f preceeded by #
+ * @param {number} alpha A number [0,1]
+ * From https://stackoverflow.com/a/28056903
+ */
+const Hex2Rgba = function(hex, alpha) {
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) {
+    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+  } else {
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
+};
+
 const StrokeObj = function(_input) {
   let input = _input || {};
   this.width = _input.width || 1;
   this.color = _input.color || '#000000';
   this.alpha = _input.alpha || 1;
+  this.gradient = _input.gradient || null;
 };
 
 const FillObj = function(_input) {
   let input = _input || {};
   this.color = _input.color || '#ffffff';
   this.alpha = _input.alpha || 0.5;
-  this.direction = _input.direction || 'vertical';
-  this.gradient = [
-    { color: '#000000', alpha: 1, loc: 0.5 },
-    { color: '#ffffff', alpha: 1, loc: 0.75 }
-  ];
+  this.gradient = _input.gradient || null;
 };
 
 const FontObj = function(_input) {
